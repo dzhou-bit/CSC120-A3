19b2c354d8235bb8644555b4e226fcce8ecf8eb0
dzhou-bit <dzhou@smith.edu>
2026-02-18 17:16:51 -0500

    final version

--- Conversation.java
+++ Conversation.java
@@ -1,13 +1,27 @@
 // You should **not** update any call signatures in this file
 // only modify the body of each function
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+
 class Conversation implements ConversationRequirements {
 
   // Attributes 
+  private Scanner scanner;
+  private ArrayList<String> transcript;
+  private int rounds;
+  private Random random;
+
 
   /**
    * Constructor 
    */
   Conversation() {
+    scanner = new Scanner(System.in);
+    transcript = new ArrayList<>();
+    random = new Random();
     
   }
 
@@ -15,6 +29,27 @@ class Conversation implements ConversationRequirements {
    * Starts and runs the conversation with the user
    */
   public void chat() {
+    System.out.print("How many rounds?");
+    rounds = scanner.nextInt();
+    scanner.nextLine();
+
+    String greeting = "Hi there! What's on your mind?";
+    System.out.println("\n"+ greeting);
+    transcript.add(greeting);
+
+    for (int i = 0; i< rounds; i++){
+      String userInput = scanner.nextLine();
+      transcript.add(userInput);
+
+      String response = respond(userInput);
+      System.out.println(response);
+      transcript.add(response);
+  
+    }
+
+    String goodbye = "See ya!";
+    System.out.println(goodbye);
+    transcript.add(goodbye);
 
   }
 
@@ -22,6 +57,10 @@ class Conversation implements ConversationRequirements {
    * Prints transcript of conversation
    */
   public void printTranscript() {
+    System.out.println("\nTranscript:");
+    for (String line: transcript){
+      System.out.println(line);
+    }
 
   }
 
@@ -31,15 +70,52 @@ class Conversation implements ConversationRequirements {
    * @return mirrored or canned response to user input  
    */
   public String respond(String inputString) {
-    String returnString = ""; 
-    return returnString; 
+    String[] words = inputString.split(" ");
+    boolean mirrored = false;
+
+    for (int i = 0; i < words.length; i++) {
+      String w = words[i].toLowerCase();
+
+      if (w.equals("i")) {
+        words[i] = "you";
+        mirrored = true;
+      } else if (w.equals("me")) {
+        words[i] = "you";
+        mirrored = true;
+      } else if (w.equals("am")) {
+        words[i] = "are";
+        mirrored = true;
+      } else if (w.equals("you")) {
+        words[i] = "I";
+        mirrored = true;
+      } else if (w.equals("my")) {
+        words[i] = "your";
+        mirrored = true;
+      } else if (w.equals("your")) {
+        words[i] = "my";
+        mirrored = true;
+      }
+    }
+
+    if (mirrored) {
+      return String.join(" ", words) + "?";
+    }
+
+    String[] canned = {
+      "Mmm-hm.",
+      "Tell me more.",
+      "Interesting.",
+      "I see.",
+      "Go on."
+    };
+
+    return canned[random.nextInt(canned.length)];
   }
 
   public static void main(String[] arguments) {
-
     Conversation myConversation = new Conversation();
     myConversation.chat();
     myConversation.printTranscript();
-
   }
 }
+
--- reflection.md
+++ reflection.md
@@ -5,3 +5,5 @@ Use this file to record your reflection on this assignment.
 - What are your initial impressions of Java? 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+Because I started this assignment a bit late, so I wasn't able to work with my classmates or to attend office hours. My initial impression about java is that it has a quite different syntax from Python which I was more farmiliar to. When I was working on this assignment, it takes some time to fix the syntax. For the difference between the two languages, I think java is more explicit in terms of declaring variable types. One advice from me is to start assignments early and talk to people for initial thinking, which I believe will be easier.
